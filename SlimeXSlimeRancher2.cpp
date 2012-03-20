41aa0cec31d2457f43aa9cf048d188db9f00d284
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-03-20 22:15:14 +0900

    add practice

---
+++ SlimeXSlimeRancher2.cpp
@@ -0,0 +1,65 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <map>
+#include <utility>
+#include <set>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <complex>
+#include <stack>
+#include <queue>
+using namespace std;
+
+static const double EPS = 1e-5;
+
+#define times(i,n) for(int i = 0; i < n; i++)
+#define rtimes(i,n) for(int i = n; i >= 0; i--)
+#define each(c,i) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define range_each(b,e,i) for(typeof(b) i=b; i!=e; ++i)
+#define find_a(x,ary) find(ary.begin(),ary.end(),x)
+#define exist_in(ary,x) find(ary.begin(),ary.end(),x) != ary.end()
+
+class SlimeXSlimeRancher2 {
+public:
+  int train(vector <int> attributes) {
+    int max;
+    int result=0;
+
+    sort(attributes.begin(),attributes.end());
+    max = attributes.back();
+
+    each(attributes,i)
+      result += abs(max - *i);
+
+    return result;
+  }
+
+  
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1,2,3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; verify_case(0, Arg1, train(Arg0)); }
+	void test_case_1() { int Arr0[] = {5,5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(1, Arg1, train(Arg0)); }
+	void test_case_2() { int Arr0[] = {900,500,100}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1200; verify_case(2, Arg1, train(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main() {
+  SlimeXSlimeRancher2 ___test;
+  ___test.run_test(-1);
+}
+// END CUT HERE
